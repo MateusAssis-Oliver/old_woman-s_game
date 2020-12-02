52abba8b820971c4f0184741fd91e9ee18b5e7db
Mateus De Assis <aln13084.1@gmail.com>
2020-12-02 10:48:42 -0300

    fazendo a incersão dos icones

--- script/Script_old_woman-s_game.js
+++ script/Script_old_woman-s_game.js
@@ -1,15 +1,34 @@
+function circulo(parametro) {
+
+    let idDiv = '#' + parametro;
+
+    $(idDiv).append(`<div id='circulo'></div>`)
+
+    let idDivC = '#' + $(`${idDiv} div`).attr('id')
+
+    $(idDivC).css({
+        'width': '50px',
+        'height': '50px',
+        'background': 'blue',
+        'borderRadius': '1000px',
+        'margin': ' 50% auto'
+    })
+
+    return 0;
+}
+
+
+
 $(document).ready(function() {
 
     //1- pegando o id da classe clicada
     //2 - monitorando todas as divs com a classe casa
     $(".casa").click(function() {
-
         // capturando o id da classe clicada
         /* id recebe a class casa como o  attr 
         que retorna o valor do elemento atribuido a ele como o id  */
         let id = $(this).attr("id")
-
-        $('#' + id).css('background', 'red')
+        circulo(id)
         console.log(id);
 
     });
