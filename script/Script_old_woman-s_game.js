6fb771a421cee7691d71699fd7b7045acb15006e
MATEUS ASSIS <aln13084.1@gmail.com>
2021-04-05 16:05:04 -0300

    Add da função para verificar o vencedor

--- script/Script_old_woman-s_game.js
+++ script/Script_old_woman-s_game.js
@@ -4,33 +4,42 @@ const old_woman_s_game = {
     //Simplismente add a imagem no quadro selecionado
     addImg: (parametro, jogador) => {
 
+        let jog = jogador;
+
         /* add o # para poder trabalhar com o id */
         let idDiv = '#' + parametro;
+
         let img = $(`${idDiv} img `).attr('id');
 
         //controle, para evitar loops de imgs
         if (img === `img_${parametro}`) { old_woman_s_game.removeImg(img); return 0; }
 
         //adiona a img na div cricada
-        $(idDiv).append(`<img src="../imgs/O.png" alt="" calss='${jogador}'id='img_${parametro}' >`);
+        $(idDiv).append(`<img src="../imgs/O.png" alt="1" calss='${jogador}'id='img_${parametro}' >`);
 
         //chamada de func para reajustar a img na div
         old_woman_s_game.image_scaling(idDiv);
-        old_woman_s_game.VerificarVencedor(idDiv);
+
+        
+
     },
 
     //reescreve a escala da imagem para não utrappasar as linhas
     image_scaling: (idDiv) => {
 
         let idImg = '#' + $(`${idDiv} img`).attr('id');
+        
 
         /* modifica o tamanho da imagem original para não ocorre um overflow*/
         $(`${idImg}`).css({ 'max-height': '100%', 'max-width': '100%', 'margin-top': '50px' });
 
+        old_woman_s_game.VerificarVencedor(idImg);
     },
     vezJogagdor: () => {
 
+
     },
+
     /* parametro recebe a casa clicada */
     removeImg: (parametro) => {
         /* remove a img da casa clicada  */
@@ -38,12 +47,27 @@ const old_woman_s_game = {
         $(idImg).remove();
 
     },
-    
+
     /* chamada da função feito dentro da func addImg */
-    VerificarVencedor: (parametro) => {
+    VerificarVencedor: (parametro) => { 
+        let numCasa = 9;
+        let aux = parametro;
+      
+        //id das img da casa clicada
+        console.log(aux)
 
+        /* pecorre as casas */
+        /* Pura magica ne, sei... */
+        for(let i = 1; i <= numCasa; i++){
 
-
+            //usa o indice para pecorre as casas
+            let casa = "#casa" + `${i}`;
+            
+            //entra na csa e busca o id da  img dela
+            let teste = $(`${casa} img`).attr("id")
+    
+            console.log(teste)
+        }
     }
 }
 
@@ -52,11 +76,17 @@ $(document).ready(function () {
     //1- pegando o id da classe clicada
     //2 - monitorando todas as divs com a classe casa
     $(".casa").click(function () {
+
         // capturando o id da classe clicada
+
         /* id recebe a class casa como o  attr 
         que retorna o valor do elemento atribuido a ele como o id  */
-        let id = $(this).attr("id")
-        old_woman_s_game.addImg(id)
+        let id = $(this).attr("id");
+
+
+     // let jogador = vezJogagdor("O");
+
+        old_woman_s_game.addImg(id,"O");
 
 
     });
