4c56e3797d66bdd86ec06ed02f9bdf1a41e4e89f
MATEUS ASSIS <aln13084.1@gmail.com>
2021-04-06 13:13:32 -0300

    script criado para organização

---
+++ script/main-Script_old_woman-s_game.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+
+    //1- pegando o id da classe clicada
+    //2 - monitorando todas as divs com a classe casa
+    $(".casa").click(function () {
+
+        // capturando o id da classe clicada
+
+        /* id recebe a class casa como o  attr 
+        que retorna o valor do elemento atribuido a ele como o id  */
+        let id = $(this).attr("id");
+
+
+     // let jogador = vezJogagdor("O");
+
+        old_woman_s_game.addImg(id,"O");
+
+    });
+});
