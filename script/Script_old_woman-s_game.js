9dd24ffcdad0172e0d61095cc25cc37da1529456
MATEUS ASSIS <aln13084.1@gmail.com>
2021-04-16 16:45:28 -0300

    levando o jogo para uma matris array

--- script/Script_old_woman-s_game.js
+++ script/Script_old_woman-s_game.js
@@ -26,7 +26,7 @@ const old_woman_s_game = {
     image_scaling: (idDiv) => {
 
         let idImg = '#' + $(`${idDiv} img`).attr('id');
-        
+
 
         /* modifica o tamanho da imagem original para não ocorre um overflow*/
         $(`${idImg}`).css({ 'max-height': '100%', 'max-width': '100%', 'margin-top': '50px' });
@@ -47,24 +47,60 @@ const old_woman_s_game = {
     },
 
     /* chamada da função feito dentro da func addImg */
-    VerificarVencedor: (parametro) => { 
+    VerificarVencedor: (parametro) => {
+
         let numCasa = 9;
         let aux = parametro;
-      
+        let arrayJogo = [];
+
         //id das img da casa clicada
-        console.log(aux)
+        console.log(` id das img da casa clicada : ${aux}`);
 
         /* pecorre as casas */
         /* Pura magica ne, sei... */
-        for(let i = 1; i <= numCasa; i++){
+        for (let i = 1; i <= numCasa; i++) {
 
             //usa o indice para pecorre as casas
             let casa = "#casa" + `${i}`;
-            
+
             //entra na csa e busca o id da  img dela
             let teste = $(`${casa} img`).attr("id")
-    
+
+            teste != undefined ? arrayJogo[i] = teste : '';
             console.log(teste)
-        }
+        };
+
+        console.log('array jogo : '+ arrayJogo); 
+       quebraArray(arrayJogo);
     }
+};
+
+function quebraArray(array){
+
+    console.log('Array f-quebraArray : ' + array)
+    let cont = 1;
+
+    let arrayAux=[];
+    //CRINADO UMA MATRIS TIPO JOGO DA VELHA
+    for (let i = 1; i <= 3; i++) { arrayAux[i] = []; };
+    console.table(arrayAux);
+
+    //linha
+    for(let indexLinha = 1; indexLinha <= 3; indexLinha++){
+
+        //coluna
+        for(let indexcoluna = 1; indexcoluna <= 3; indexcoluna++,cont ++ ){
+            console.log("cont" + cont)
+            arrayAux[indexLinha][indexcoluna] = array[cont];
+
+            console.log('arrai[cont] = ' + array[cont]);
+            
+
+            
+        };
+    };console.table(arrayAux);
+        
+
+
+
 };
